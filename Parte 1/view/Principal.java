964f5b609231318846868ee0d08171b30a6aceeb
Gabriel Henrique <103072636+GabrielHenrique20@users.noreply.github.com>
2025-10-12 21:47:19 -0300

    Add files via upload

---
+++ Parte 1/view/Principal.java
@@ -0,0 +1,50 @@
+package view;
+import java.util.Scanner;
+import controller.MenuViagem;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int opcao;
+
+        try {
+            do {
+                System.out.println("\n\nAEDsIII");
+                System.out.println("-------");
+                System.out.println("> Início");
+                System.out.println("\n1 - Inserir viagem");
+                System.out.println("0 - Sair");
+
+                System.out.print("\nOpção: ");
+                try {
+                    opcao = Integer.valueOf(console.nextLine());
+                } catch (NumberFormatException e) {
+                    opcao = -1;
+                }
+
+                switch (opcao) {
+                    case 1:
+                        MenuViagem menuViagem = new MenuViagem();
+                        menuViagem.menu();
+                        break;
+                    case 0:
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
+            } while (opcao != 0);
+
+        } catch (Exception e) {
+            System.err.println("Erro fatal no sistema:");
+            e.printStackTrace();
+        } finally {
+            console.close();
+        }
+    }
+}
+
+
+//javac -d out $(find src -name "*.java") 
+//java -cp out src.views.Principal
