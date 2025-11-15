a829fd57bbea3bf29f146012ce3e6875a368403f
Gabriel Henrique <103072636+GabrielHenrique20@users.noreply.github.com>
2025-11-15 13:41:15 -0300

    Delete Parte 3 directory

--- Parte 3/TP3/SistemaViagens/src/model/RegistroAtividade.java
+++
@@ -1,9 +0,0 @@
-package model;
-import java.io.IOException;
-
-public interface RegistroAtividade {
-    public void setId(int i);
-    public int getId();
-    public byte[] toByteArray() throws IOException;
-    public void fromByteArray(byte[] b) throws IOException;
-}
